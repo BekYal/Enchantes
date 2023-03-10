12c1f91485c71d312211cd30b47ec8e910a9a515
hygohy@gmail.com <hygohy@gmail.com>
2023-03-11 01:03:30 +0300

    da 2

--- bigHuy/EnchantesHelper.js
+++ bigHuy/EnchantesHelper.js
@@ -1,43 +1,13 @@
-/*
-BUILD INFO:
-  dir: core/dev
-  target: core/littleHuy.js
-  files: 3
-*/
+LIBRARY({
+    name: "EnchantsHelper",
+    version: 1,
+    shared: true,
+    api: "CoreEngine"
+});
 
-
-
-// file: Header/Values.js
-
-const WEAPON = 16 | 32 | 512;
-const TOOLS = 16 | 512 | 64 | 256 | 1024 | 128 | 2048;
-const ARMORS = 1 | 8 | 4 | 2;
-const FOOT = 4 | 2;
-const ARM_SLOTS = 3 | 2 | 1 | 0;
-
-/*const Values = {
-    NotEntBite: 95 | 61 | 80 | 90 | 98 | 100 | 79 | 82 | 71 | 87 | 69 | 68 | 66 | 70 | 85 | 72 | 77 | 96 | 64 | 88 | 93 | 101 | 84 | 67 | 83 | 76 | 94 | 81 | 86 | 73 | 97 | 105,
-    weapon: 16 | 32 | 512,
-    foot: 4 | 2,
-    tools: 16 | 512 | 64 | 256 | 1024 | 128 | 2048,
-    armors: 1 | 8 | 4 | 2,
-    arm_slots: 0 | 1 | 2 | 3
-};*/
-
-
-
-
-// file: Enchantes.js
-
-
-
-
-
-// file: EnchantsAPI.js
-
-IDRegistry.genItemID("enchanBook");
-Item.createItem("enchanBook", "enchantment book", { name: "book_enchanted" }, { stack: 64 });
-
+IDRegistry.genItemId("enchanBook");
+Item.createItem("enchanBook", "enchantment book", { name: "book_enchanted" }, { stack: 1 });
+Item.getItemByID(ItemID.enchanBook).setEnchantType(MASK.ALL);
 var MASK = {
 	AXE: 512,
 	ALL: 16383,
@@ -59,15 +29,8 @@ var MASK = {
 };
 
 let Curses = [28, 27];
-const Enchants = {
-	removeCurses: function() {
-		for (var i = 0; i < Curses.length; i++) {
-			if (Curses[i] === enchant) {
 
-			}
-		}
-		return false
-	},
+const Enchants = {
 	getCurses: function() {
 		return Curses;
 	},
@@ -160,44 +123,10 @@ const Enchants = {
 			}
 		});
 	}
-};
+};	
 
-let LiveSteal = CustomEnchant.newEnchant("LiveSteal", Translation.translate("LiveSteal"))
-	.setMinMaxLevel(1, 3).setMask(MASK.ALL);
-	LiveSteal.setFrequency(1);
-	
-
-Enchants.addBook(LiveSteal.id, 3);
-
-Enchants.hurtOwner(LiveSteal.id, function (item, enchantLevel, attacker, victim, damageValue, damageType){
-	Game.message("h");
-}, 3);
-let huy = 0;
-Callback.addCallback("ItemUse", function(coords, item, block, isExternal, player){
-	if (item.id == ItemID.enchanBook) {
-		huy = 1;
-	}
-});
-
-Callback.addCallback("ServerPlayerTick", function(player) {
-	let item = Entity.getCarriedItem(player);
-	if (item.extra && huy == 1) {
-	let enchArr = item.extra.getEnchants();
-	let removeEnch = [];
-	removeEnch = enchArr.filter(function(e) {
-		!~Curses.indexOf(e);
-	});
-	for(let i = 0; i <= removeEnch.length; i++){
-		item.extra.removeEnchant(removeEnch[i]);
-	}
-	}
-		
-});
-
-Enchants.hurt(LiveSteal.id, function(item, enchantLevel, attacker, victim, damageValue, damageType) {
-	Game.message(item.extra + "");
-}, 3);
-
-
+EXPORT("Enchants", Enchants);
+EXPORT("MASK", MASK);
+EXPORT("Cursee", Curses);
 
 
