37c719f880c753ba45caaa590008c385d8a07482
hygohy@gmail.com <hygohy@gmail.com>
2023-03-23 23:50:53 +0300

    доработал UnionToWater

--- core/dev/Enchants.js
+++ core/dev/Enchants.js
@@ -1,40 +1,82 @@
+function inWater(playerr) {
+	let pos = Entity.getPosition(playerr);
+	pos = {
+		x: Math.floor(pos.x + .25),
+		z: Math.floor(pos.z + .25),
+		y: Math.floor(pos.y - .25),
+	};
+	if (World.getBlockID(pos.x, pos.y, pos.z) == 9 || World.getBlockID(pos.x, pos.y, pos.z) == 8) { return true } else { return false }
+}
 IMPORT("EnchantsHelper");
 
 var Stunning = CustomEnchant.newEnchant("Stunning", Translation.translate("Stunning"))
 	.setMinMaxCost(5, 10, 5, 10)
 	.setMinMaxLevel(1, 3)
 	.setMask(Mask.weapons)
-	.setFrequency(1);
-	
+	.setFrequency(4);
+
 var UnionToWater = CustomEnchant.newEnchant("UnionToWater", Translation.translate("Union to water"))
-	.setMinMaxCost(1, 3, 1, 3)
-	.setMinMaxLevel(1, 1)
+	.setMinMaxCost(5, 10, 5, 10)
+	.setMinMaxLevel(1, 3)
 	.setMask(Mask.armor)
-	.setFrequency(1);
+	.setFrequency(4);
 
 var Blunt = CustomEnchant.newEnchant("Blunt", Translation.translate("Blunt"))
 	.setMinMaxCost(1, 3, 1, 3)
 	.setMinMaxLevel(1, 3)
 	.setMask(Mask.tool)
-	.setFrequency(1);
+	.setFrequency(2);
+	
+Enchants.addBook(Blunt.id);
+Enchants.addBook(UnionToWater.id);
+Enchants.addBook(Stunning.id);
 
+Enchants.onNaked(UnionToWater.id, function(item, enchantLevel, player) {
+	let pos = Entity.getPosition(player);
+	let rt = BlockSource.getDefaultForActor(player);
+	if ( inWater(player) || (rt.canSeeSky(pos.x, pos.y, pos.z) && (World.getWeather().thunder || World.getWeather().rain ) ) ) {
+		switch (enchantLevel) {
+			case 1:
+				if (World.getThreadTime() % 100 == 0) {
+					Entity.addEffect(player, 1, 1, 100, false, false);
+					Entity.addEffect(player, 5, 1, 100, false, false);
+					Entity.addEffect(player, 3, 1, 100, false, false);
+				}
+				break;
+			case 2:
+				if (World.getThreadTime() % 100 == 0) {
+					Entity.addEffect(player, 1, 3, 100, false, false);
+					Entity.addEffect(player, 5, 3, 100, false, false);
+					Entity.addEffect(player, 3, 3, 100, false, false);
+				}
+				break;
+			case 3:
+				if (World.getThreadTime() % 100 == 0) {
+					Entity.addEffect(player, 1, 3, 100, false, false);
+					Entity.addEffect(player, 5, 3, 100, false, false);
+					Entity.addEffect(player, 3, 2, 100, false, false);
+				}
+				break;
+		}
+	}
+});
 
 Enchants.hurt(Stunning.id, function(item, enchantLevel, attacker, victim, damageValue, damageType) {
 	switch (enchantLevel) {
 		case 1:
-			Chance.executeWithPercentChance(10, function(){
+			Chance.executeWithPercentChance(10, function() {
 				Entity.addEffect(victim, 2, 30, 60, false, false);
 				Entity.addEffect(victim, 18, 30, 60, false, false);
 			});
 			break;
 		case 2:
-			Chance.executeWithPercentChance(15, function(){
+			Chance.executeWithPercentChance(15, function() {
 				Entity.addEffect(victim, 2, 30, 80, false, false);
 				Entity.addEffect(victim, 18, 30, 80, false, false);
 			});
 			break;
 		case 3:
-			Chance.executeWithPercentChance(30, function(){
+			Chance.executeWithPercentChance(30, function() {
 				Entity.addEffect(victim, 2, 30, 100, false, false);
 				Entity.addEffect(victim, 18, 30, 100, false, false);
 			});
@@ -42,20 +84,20 @@ Enchants.hurt(Stunning.id, function(item, enchantLevel, attacker, victim, damage
 	}
 });
 
-Enchants.destroyBlock(Blunt.id, function(item, enchantLevel, coords, block, player){
+Enchants.destroyBlock(Blunt.id, function(item, enchantLevel, coords, block, player) {
 	switch (enchantLevel) {
 		case 1:
-			Chance.executeWithPercentChance(10, function(){
+			Chance.executeWithPercentChance(10, function() {
 				Game.prevent();
 			});
 			break;
 		case 2:
-			Chance.executeWithPercentChance(30, function(){
+			Chance.executeWithPercentChance(30, function() {
 				Game.prevent();
 			});
 			break;
 		case 3:
-			Chance.executeWithPercentChance(50, function(){
+			Chance.executeWithPercentChance(50, function() {
 				Game.prevent();
 			});
 			break;
@@ -82,89 +124,7 @@ Enchants.destroyBlock(Blunt.id, function(item, enchantLevel, coords, block, play
 });
 
 Enchants.setCurse(Blunt.id);*/
-
-IMPORT("EnchantsHelper");
-
-var Stunning = CustomEnchant.newEnchant("Stunning", Translation.translate("Stunning"))
-	.setMinMaxCost(5, 10, 5, 10)
-	.setMinMaxLevel(1, 3)
-	.setMask(Mask.weapons)
-	.setFrequency(1);
-	
-var UnionToWater = CustomEnchant.newEnchant("UnionToWater", Translation.translate("Union to water"))
-	.setMinMaxCost(1, 3, 1, 3)
-	.setMinMaxLevel(1, 1)
-	.setMask(Mask.armor)
-	.setFrequency(1);
-
-var Blunt = CustomEnchant.newEnchant("Blunt", Translation.translate("Blunt"))
-	.setMinMaxCost(1, 3, 1, 3)
-	.setMinMaxLevel(1, 3)
-	.setMask(Mask.tool)
-	.setFrequency(1);
-
-
-Enchants.hurt(Stunning.id, function(item, enchantLevel, attacker, victim, damageValue, damageType) {
-	switch (enchantLevel) {
-		case 1:
-			Chance.executeWithPercentChance(10, function(){
-				Entity.addEffect(victim, 2, 30, 60, false, false);
-				Entity.addEffect(victim, 18, 30, 60, false, false);
-			});
-			break;
-		case 2:
-			Chance.executeWithPercentChance(15, function(){
-				Entity.addEffect(victim, 2, 30, 80, false, false);
-				Entity.addEffect(victim, 18, 30, 80, false, false);
-			});
-			break;
-		case 3:
-			Chance.executeWithPercentChance(30, function(){
-				Entity.addEffect(victim, 2, 30, 100, false, false);
-				Entity.addEffect(victim, 18, 30, 100, false, false);
-			});
-			break;
-	}
+Callback.addCallback("ServerPlayerTick", function(player) {
+	let pos = Entity.getPosition(player);
+	Game.tipMessage(World.getBlockID(pos.x, pos.y, pos.z) + '');
 });
-
-Enchants.destroyBlock(Blunt.id, function(item, enchantLevel, coords, block, player){
-	switch (enchantLevel) {
-		case 1:
-			Chance.executeWithPercentChance(10, function(){
-				Game.prevent();
-			});
-			break;
-		case 2:
-			Chance.executeWithPercentChance(30, function(){
-				Game.prevent();
-			});
-			break;
-		case 3:
-			Chance.executeWithPercentChance(50, function(){
-				Game.prevent();
-			});
-			break;
-	}
-});
-
-/*
-Enchants.inInv(Blunt.id, function(item, enchantLevel, player){
-	switch (enchantLevel) {
-		case 1:
-			Game.message("1 level");
-			break;
-		case 2:
-			Game.message("2 level");
-			break;
-	}
-});
-/*
-
-Enchants.destroyBlock(Blunt.id, function(item, enchantLevel, coords, block, player){
-	Chance.executeWithPercentChance(50, function(){
-	Game.prevent();
-	});
-});
-
-Enchants.setCurse(Blunt.id);*/
-
